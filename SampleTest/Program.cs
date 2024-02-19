30cd16fc9b5a0022c306a80fb7c1962c9cb9fe1c
srini <srini@LAPTOP-PH3S57VA>
2024-02-19 13:24:52 +0530

    Add project files.

---
+++ SampleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
